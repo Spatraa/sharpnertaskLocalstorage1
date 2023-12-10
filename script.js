79608e06971c534284e504f9a68410ae33a1468c
sankar patra <sankarpatra573@gmail.com>
2023-12-10 13:48:44 +0530

    do

--- script.js
+++ script.js
@@ -1,66 +1,72 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const userForm = document.getElementById('userForm');
-    const userDataDiv = document.getElementById('userData');
-  
-    userForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      // Get form values
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const phone = document.getElementById('phone').value;
-  
-      // Create user object
-      const userDetails = {
-        name: name,
-        email: email,
-        phone: phone
-      };
-  
-      // Check if localStorage already has user details
-      const storedUsers = localStorage.getItem('users');
-      const usersArray = storedUsers ? JSON.parse(storedUsers) : [];
-  
-      // Add new user details to the array
+  const userForm = document.getElementById('userForm');
+  const userDataDiv = document.getElementById('userData');
+  let editingIndex = -1; // To keep track of the index being edited
+
+  userForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    const userDetails = {
+      name: name,
+      email: email,
+      phone: phone
+    };
+
+    const storedUsers = localStorage.getItem('users');
+    const usersArray = storedUsers ? JSON.parse(storedUsers) : [];
+
+    if (editingIndex > -1) {
+      // If editing, update the user details at the editing index
+      usersArray[editingIndex] = userDetails;
+      editingIndex = -1; // Reset editingIndex after update
+    } else {
+      // If not editing, add new user details to the array
       usersArray.push(userDetails);
-  
-      // Save updated array to localStorage
-      localStorage.setItem('users', JSON.stringify(usersArray));
-  
-      // Display user data in the browser
-      renderUsers();
-      
-      // Reset form fields
-      userForm.reset();
-    });
-  
-    // Display existing user data from localStorage on page load
-    renderUsers();
-  
-    function renderUsers() {
-      userDataDiv.innerHTML = ''; // Clear previous data
-  
-      const storedUsers = localStorage.getItem('users');
-      if (storedUsers) {
-        const usersArray = JSON.parse(storedUsers);
-        usersArray.forEach((user, index) => {
-          const userItem = document.createElement('ul');
-          const deleteButton = document.createElement('button');
-          deleteButton.textContent = 'Delete';
-          
-          // Add event listener for delete button
-          deleteButton.addEventListener('click', function() {
-            // Remove user from array and update localStorage
-            usersArray.splice(index, 1);
-            localStorage.setItem('users', JSON.stringify(usersArray));
-            renderUsers(); // Re-render the updated user list
-          });
-  
-          userItem.innerHTML = `<li>Name: ${user.name} | Email: ${user.email} | Phone: ${user.phone}</li>`;
-          userItem.appendChild(deleteButton);
-          userDataDiv.appendChild(userItem);
-        });
-      }
     }
+
+    localStorage.setItem('users', JSON.stringify(usersArray));
+    renderUsers();
+    userForm.reset();
   });
-  
+
+  renderUsers();
+
+  function renderUsers() {
+    userDataDiv.innerHTML = '';
+
+    const storedUsers = localStorage.getItem('users');
+    if (storedUsers) {
+      const usersArray = JSON.parse(storedUsers);
+      usersArray.forEach((user, index) => {
+        const userItem = document.createElement('ul');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit'; // Corrected setting textContent for editButton
+        
+        deleteButton.addEventListener('click', function() {
+          usersArray.splice(index, 1);
+          localStorage.setItem('users', JSON.stringify(usersArray));
+          renderUsers();
+        });
+
+        editButton.addEventListener('click', function() {
+          editingIndex = index; // Set the index being edited
+          const { name, email, phone } = usersArray[index];
+          document.getElementById('name').value = name;
+          document.getElementById('email').value = email;
+          document.getElementById('phone').value = phone;
+        });
+
+        userItem.innerHTML = `<li>Name: ${user.name} | Email: ${user.email} | Phone: ${user.phone}</li>`;
+        userItem.appendChild(deleteButton);
+        userItem.appendChild(editButton); // Append edit button
+        userDataDiv.appendChild(userItem);
+      });
+    }
+  }
+});
